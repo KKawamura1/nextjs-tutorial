98254adc809a26d0ab1faee1035a8e2e02421877
Keigo Kawamura <kkawamura@preferred.jp>
2020-12-06 11:28:17 +1200

    Extract getPostData

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -15,6 +15,21 @@ export type PostData = {
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
+const getPostData = (id: string): PostData => {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the id
+  return {
+    id,
+    ...(matterResult.data as _PostData)
+  }
+}
+export { getPostData };
+
 const getSortedPostsData: () => PostData[] = () => {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
@@ -22,19 +37,9 @@ const getSortedPostsData: () => PostData[] = () => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents)
-
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as _PostData)
-    }
-  })
+    // Get data
+    return getPostData(id);
+  });
   // Sort posts by date
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
