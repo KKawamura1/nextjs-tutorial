20d8d23f51897bca52d5572178439391444b6bfd
Keigo Kawamura <kkawamura@preferred.jp>
2020-12-06 11:13:45 +1200

    implement [id]

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,6 +1,16 @@
-import Layout from "../../components/layout"
+import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/posts";
+
+const getStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 const Post = () => {
   <Layout>...</Layout>
 };
 export default Post;
+export { getStaticPaths }
