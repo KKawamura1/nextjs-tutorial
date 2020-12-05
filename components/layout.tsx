476ee7d874ac5509a420bd8a0b932524b82eb442
Keigo Kawamura <kkawamura@preferred.jp>
2020-12-05 16:54:23 +0900

    Add scss. type is ignored

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+// @ts-ignore
+import styles from "./layout.module.scss"
 
 type Props = {};
 
 const Layout: React.FC<Props> = (props) => (
-  <div>{props.children}</div>
+  <div className={styles.container}>{props.children}</div>
 );
 export default Layout;
